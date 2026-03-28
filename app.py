b9646a5b77cbf81bdb79e7f61880d2efc5d0cc52
cgale2 <cgale2@kent.edu>
2026-03-28 15:23:25 -0400

    website design

--- app.py
+++ app.py
@@ -4,7 +4,14 @@ import streamlit as st
 # streamlit run app.py
 
 # Streamlit stuff
-st.set_page_config(page_title="Hackathon 2026", layout="wide")
+st.set_page_config(
+    page_title="GRUG", 
+    page_icon="🗿", 
+    layout="wide",
+)
 
-st.title("Welcome to Hackathon 2026")
-st.write("Use the sidebar to navigate between pages.")
+st.title(":orange[GENERATE REALLY UNBREAKABLE GIBBERISH]", text_alignment="center")
+
+st.sidebar.image("assets/logo1.png")
+
+st.image("assets/caveman_password.jpg", width="stretch")
---
+++ pages/1_game.py
@@ -0,0 +1,11 @@
+import streamlit as st
+
+st.set_page_config(
+    page_title="GRUG", 
+    page_icon="🗿", 
+    layout="wide",
+)
+
+st.title("This is the game page")
+
+st.sidebar.image("assets/logo1.png")
