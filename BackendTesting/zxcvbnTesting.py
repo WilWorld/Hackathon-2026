bdf6ef1e55386e5b9a98e084fdbbba7aa726463f
ahagger1 <ahagger1@kent.edu>
2026-03-28 22:41:45 -0400

    Improved and fixed the stats for password

--- BackendTesting/zxcvbnTesting.py
+++ BackendTesting/zxcvbnTesting.py
@@ -2,6 +2,12 @@ from zxcvbn import zxcvbn
 
 # Returns string
 def password_test(password):
-    r = zxcvbn("te$t12343😀32")
-    print("time to crack ", r["calc_time"], " | guesses ", r["guesses"], " | Score: ", r["score"])
-    return "time to crack ", r["calc_time"], " | guesses ", r["guesses"], " | Score: ", r["score"]
+    if len(password) > 72:
+        return ["Password exceeds 72 characters", "For stats, have a password under 72 characters"]
+    
+    r = zxcvbn(password)
+    time = "Time to crack: " + str(r["calc_time"])
+    guess = "Number of guesses: " + str(r["guesses"])
+    score = "Score: " + str(r["score"])
+
+    return [time, guess, score]
