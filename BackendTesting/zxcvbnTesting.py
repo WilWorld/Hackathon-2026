c9d6d8d7f2405280123b5486f0ecb8be9d16f4a2
cgale2 <cgale2@kent.edu>
2026-03-29 02:55:08 -0400

    more large numbers and splitting rule prompts

--- BackendTesting/zxcvbnTesting.py
+++ BackendTesting/zxcvbnTesting.py
@@ -13,6 +13,7 @@ def password_test(password):
 
     return [time, guess, score]
 
+# Formats time in a meaningful way
 def format_time(td):
     seconds = td.total_seconds()
     if seconds < 1e-6:   # smaller than one microsecond -> nanosecond
@@ -23,30 +24,52 @@ def format_time(td):
         return f"{seconds * 1e3:.1f} ms"
     else:                # one second or bigger         -> second
         return f"{seconds:.1f} s"       
-    
+
+# Formats guesses in a meaningful way 
 def format_guesses(guesses):
     scales = [
-        (10**63, "vigintillion"),
-        (10**60, "novemdecillion"),
-        (10**57, "octodecillion"),
-        (10**54, "septendecillion"),
-        (10**51, "sexdecillion"),
-        (10**48, "quindecillion"),
-        (10**45, "quattuordecillion"),
-        (10**42, "tredecillion"),
-        (10**39, "duodecillion"),
-        (10**36, "undecillion"),
-        (10**33, "decillion"),
-        (10**30, "nonillion"),
-        (10**27, "octillion"),
-        (10**24, "septillion"),
-        (10**21, "sextillion"),
-        (10**18, "quintillion"),
-        (10**15, "quadrillion"),
-        (10**12, "trillion"),
-        (10**9,  "billion"),
-        (10**6,  "million"),
+        (10**303, "centillion"),
+        (10**300, "novemnonagintillion"),
+        (10**297, "octononagintillion"),
+        (10**294, "septennonagintillion"),
+        (10**291, "sexnonagintillion"),
+        (10**288, "quindecennonagintillion"),
+        (10**285, "quattuordecennonagintillion"),
+        (10**282, "tredecennonagintillion"),
+        (10**279, "duodecennonagintillion"),
+        (10**276, "undecennonagintillion"),
+        (10**273, "decennonagintillion"),
+        (10**270, "nonagintillion"),
+        (10**267, "octogintillion"),
+        (10**264, "septuagintillion"),
+        (10**261, "sexagintillion"),
+        (10**258, "quinquagintillion"),
+        (10**255, "quadragintillion"),
+        (10**252, "trigintillion"),
+        (10**249, "vigintillion"),
+        (10**246, "novemdecillion"),
+        (10**243, "octodecillion"),
+        (10**240, "septendecillion"),
+        (10**237, "sexdecillion"),
+        (10**234, "quindecillion"),
+        (10**231, "quattuordecillion"),
+        (10**228, "tredecillion"),
+        (10**225, "duodecillion"),
+        (10**222, "undecillion"),
+        (10**219, "decillion"),
+        (10**216, "nonillion"),
+        (10**213, "octillion"),
+        (10**210, "septillion"),
+        (10**207, "sextillion"),
+        (10**204, "quintillion"),
+        (10**201, "quadrillion"),
+        (10**198, "trillion"),
+        (10**195, "billion"),
+        (10**192, "million"),
         (10**3,  "thousand"),
+        (10**2,  "hundred"),
+        (10**1,  "ten"),
+        (10**0,  "one"),
     ]
 
     for value, name in scales:
@@ -55,6 +78,7 @@ def format_guesses(guesses):
 
     return str(guesses)
 
+# Formats the score in a meaningful way
 def format_score(score):
     if score == 0:
         return f"{score} [very weak]"
