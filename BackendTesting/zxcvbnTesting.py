d1c609b6ef99d8a5982dad14c24121f8925e3534
ahagger1 <ahagger1@kent.edu>
2026-03-28 21:03:41 -0400

    Completed rule display

--- BackendTesting/zxcvbnTesting.py
+++ BackendTesting/zxcvbnTesting.py
@@ -1,5 +1,7 @@
 from zxcvbn import zxcvbn
 
-
-r = zxcvbn("te$t12343😀32")
-print("time to crack ", r["calc_time"], " | guesses ", r["guesses"], " | Score: ", r["score"])
+# Returns string
+def password_test(password):
+    r = zxcvbn("te$t12343😀32")
+    print("time to crack ", r["calc_time"], " | guesses ", r["guesses"], " | Score: ", r["score"])
+    return "time to crack ", r["calc_time"], " | guesses ", r["guesses"], " | Score: ", r["score"]
--- backend_testing/zxcvbnTesting.py
+++
@@ -1,5 +0,0 @@
-from zxcvbn import zxcvbn
-
-
-r = zxcvbn("te$t12343😀32")
-print("time to crack ", r["calc_time"], " | guesses ", r["guesses"], " | Score: ", r["score"])
